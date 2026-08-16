1d36bc5627c476a22c65678e8b815312e86a59c3
Roshan Jeffrin R <roshanjeffrinr@gmail.com>
2026-08-16 13:49:18 +0530

    Updated

--- web.py
+++ web.py
@@ -5,7 +5,26 @@ def port_scanner():
     target = input("Enter target hostname/IP: ").strip()
     start_port = int(input("Enter starting port: "))
     end_port = int(input("Enter ending port: "))
-
+    name = {
+        20: "FTP (File Transfer Protocol)",
+        21: "FTP (File Transfer Protocol)",
+        22: "SSH (Secure Shell)",
+        23: "Telnet",
+        25: "SMTP (Simple Mail Transfer Protocol)",
+        53: "DNS (Domain Name System)",
+        67: "DHCP (Dynamic Host Configuration Protocol)",
+        68: "DHCP (Dynamic Host Configuration Protocol)",
+        80: "HTTP (Hypertext Transfer Protocol)",
+        110: "POP3 (Post Office Protocol version 3)",
+        123: "NTP (Network Time Protocol)",
+        143: "IMAP (Internet Message Access Protocol)",
+        161: "SNMP (Simple Network Management Protocol)",
+        162: "SNMP (Simple Network Management Protocol)",
+        443: "HTTPS (Hypertext Transfer Protocol Secure)",
+        445: "SMB (Server Message Block)",
+        3389: "RDP (Remote Desktop Protocol)"
+    }
+    
     try:
         ip = socket.gethostbyname(target)
     except socket.gaierror:
@@ -25,7 +44,7 @@ def port_scanner():
         sock.close()
 
         if result == 0:
-            print(f"[+] Port {port} is OPEN")
+            print(f"[+] Port {port} : {name} is OPEN")
             open_ports.append(port)
 
     print("\n[+] Scan complete.")
