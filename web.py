2daf2098f23a43358eef7f9da088a33426d28f66
Roshan Jeffrin R <roshanjeffrinr@gmail.com>
2026-08-16 12:54:20 +0530

    Updated

--- web.py
+++ web.py
@@ -3,25 +3,30 @@ import socket, threading
 
 class web:
     def __init__ (self):
-        self.port = int(input("Enter the port to search"))
+        self.port_starting = int(input("Enter port to start search"))
+        self.port_ending = int(input("Enter port upto search"))
         self.s = socket.socket()
         self.ip = socket.gethostbyname()
 
-    def port_scanner(s, ip, port, r=1):
+    def port_scanner(s, ip, port_starting, port_ending, r=1):
         open_ports = []
-        try:
-            s.socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(1)
-            r = s.connect_ex((ip, port))
-            if r == 0:
-                con = f"Port{port} is open"
-                print(con)
-            s.close()
+        for j in range(port_starting, port_ending):
+            port = port+j
+            print(port)
+        def check_ports():
+            try:
+                s.socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(1)
+                r = s.connect_ex((ip, port))
+                if r == 0:
+                    con = f"Port{port} is open"
+                    print(con)
+                s.close()
 
+            except Exception as e:
+                pass
 
-        except Exception as e:
-            pass
-
-        for i in range(1, port):
-            print (i)
+        for i in range(port_starting, port_ending):
+            thread = threading.Thread(target=check_ports(ip, port))
+            
     
