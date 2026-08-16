feb269ba4cc9c73e16ba6e10772a4a0c5c88137b
Roshan Jeffrin R <roshanjeffrinr@gmail.com>
2026-08-16 12:27:41 +0530

    Updated

--- web.py
+++ web.py
@@ -7,8 +7,21 @@ class web:
         self.s = socket.socket()
         self.ip = socket.gethostbyname()
 
-    def port_scanner(s, ip, port):
+    def port_scanner(s, ip, port, r=1):
         open_ports = []
+        try:
+            s.socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            r = s.connect_ex((ip, port))
+            if r == 0:
+                con = f"Port{port} is open"
+                print(con)
+            s.close()
+
+
+        except Exception as e:
+            pass
+
         for i in range(1, port):
             print (i)
     
